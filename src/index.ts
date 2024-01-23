ba7aeeba5c12019fcc5afccc326b8167532b2f4a
crazywoola <li.zheng@dentsplysirona.com>
2024-01-23 10:07:46 +0800

    feat: 🚀

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { z, createRoute, OpenAPIHono } from "@hono/zod-openapi";
+
+type Bindings = {
+  OPENAPI_VERSION: string;
+  TOOL_VERSION: string;
+  TOOL_NAME: string;
+  TOOL_DESCRIPTION: string;
+};
+
+const app = new OpenAPIHono<{ Bindings: Bindings }>();
+
+app.doc31("/doc", (c) => ({
+  openapi: c.env.OPENAPI_VERSION,
+  info: {
+    version: c.env.TOOL_VERSION,
+    title: c.env.TOOL_NAME,
+    description: c.env.TOOL_DESCRIPTION,
+  },
+  servers: [{ url: new URL(c.req.url).origin }],
+}));
+
+const reqSchema = z.object({
+  count: z.number().openapi({ example: 1, description: "Number of quotes to get" }),
+});
+
+const resSchema = z.object({
+  result: z.string().openapi({ example: "ok" }),
+});
+
+const quoteRoute = createRoute({
+  method: "post",
+  path: "/quotes",
+  summary: "Get quotes from Breaking Bad",
+  description: "Retrieve quotes from the Breaking Bad series",
+  operationId: "GetQuotesFromBreakingBad",
+  request: {
+    body: {
+      content: {
+        "application/json": { schema: reqSchema },
+      },
+    },
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": { schema: resSchema },
+      },
+      description: "OK",
+    },
+  },
+});
+
+app.openapi(quoteRoute, async (c) => {
+  const { count } = c.req.valid("json");
+  const url = `https://api.breakingbadquotes.xyz/v1/quotes/${count}`;
+  const result = await fetch(url).then((res) => res.text());
+  return c.json({ result });
+});
+
+export default app;
